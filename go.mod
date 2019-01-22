2d3a46fe3229ea355306e0492be7419efcb74e5c
yu <yu@yus-MacBook-Pro.local>
2019-01-22 11:09:36 -0800

    added more

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/netmsglog/goutil
+
+require github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d
--- util.go
+++ util.go
@@ -1,8 +1,16 @@
 package goutil
 
 import (
+	"crypto/md5"
+	b64 "encoding/base64"
+	"encoding/hex"
+	"io"
 	"io/ioutil"
+	"os"
+	"strconv"
 	"strings"
+
+	"github.com/nu7hatch/gouuid"
 )
 
 func Trim(str string) string {
@@ -21,10 +29,62 @@ func Str2Arr(str string) []string {
 	return words
 }
 
+func FileExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	return false
+}
+
 func File2Str(fn string) string {
 	f, err := ioutil.ReadFile(fn)
 	if err != nil {
 		return ""
 	}
 	return Trim(string(f))
+}
+
+func INT(str string) int {
+	i, _ := strconv.Atoi(str)
+	return i
+}
+
+func UUID() string {
+	uu, _ := uuid.NewV4()
+	return uu.String()
+}
+
+func MD5(str string) string {
+	h := md5.New()
+	io.WriteString(h, str)
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func Segment(str string, b string, e string) string {
+	pos := strings.Index(str, b)
+	if pos == -1 {
+		return ""
+	}
+	npos := pos + len(b)
+	if e == "" {
+		return str[npos:]
+	}
+	epos := strings.Index(str[npos:], e)
+	if epos == -1 {
+		return str[npos:]
+	}
+	return str[npos:(npos + epos)]
+}
+
+func Enc64(s string) string {
+	return b64.StdEncoding.EncodeToString([]byte(s))
+}
+
+func Dec64(s string) string {
+	b, err := b64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return s
+	}
+	return string(b)
 }
